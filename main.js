b96bbda70270c960680b33541381bc327b2e1e2c
Brian Nixon <btn10@pitt.edu>
2020-02-14 21:22:58 -0500

    First commit for JS

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("Code Running here");
+
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 400;
+
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo('body');
+
+var FPS = 30;
+setInterval(function() {
+    PaymentRequestUpdateEvent();
+    draw();
+}, 1000/FPS);
+
+function update() { 
+    //comment
+ }
+function draw() { 
+    //comment
+ }
